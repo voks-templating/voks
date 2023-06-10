2f269aa7c8c7cef0bec6b1df55cc4a45733a40f2
Falk Hoppe <falk@hey.com>
2023-06-10 12:06:08 +0200

    test: add missing tests for template attribute

---
+++ lib/template_attribute_test.ts
@@ -0,0 +1,34 @@
+import { assertEquals } from "asserts";
+import { TemplateAttribute } from "./template_attribute.ts";
+
+Deno.test("escapeHTML", async (t) => {
+  await t.step("boolean attribute", () => {
+    const attribute = new TemplateAttribute("data-fubar", true);
+    assertEquals(attribute.isBoolean, true);
+    assertEquals(attribute.toString(), "data-fubar ");
+  });
+
+  await t.step("string attribute", () => {
+    const attribute = new TemplateAttribute("data-fubar", "hello, world!");
+    assertEquals(attribute.isBoolean, false);
+    assertEquals(attribute.toString(), 'data-fubar="hello, world!" ');
+  });
+
+  await t.step("number attribute", () => {
+    const attribute = new TemplateAttribute("data-fubar", 7);
+    assertEquals(attribute.isBoolean, false);
+    assertEquals(attribute.toString(), 'data-fubar="7" ');
+  });
+
+  await t.step("null attribute", () => {
+    const attribute = new TemplateAttribute("data-fubar", null);
+    assertEquals(attribute.isBoolean, false);
+    assertEquals(attribute.toString(), "");
+  });
+
+  await t.step("undefined attribute", () => {
+    const attribute = new TemplateAttribute("data-fubar", undefined);
+    assertEquals(attribute.isBoolean, false);
+    assertEquals(attribute.toString(), "");
+  });
+});
