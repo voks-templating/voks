328741b5298f83492f4fcfcf1f65f74a0c03c980
Falk Hoppe <falk@hey.com>
2023-06-10 11:55:41 +0200

    refactor: improved perf of escaping method

--- lib/escape_html.ts
+++ lib/escape_html.ts
@@ -1,8 +1,9 @@
+// benched the replace(//) method against replaceAll (it was faster for a simple string in about 60ns)
 export const escapeHTML = (htmlText = ""): string => {
   return htmlText.toString()
-    .replaceAll("&", "&amp;") // has to be first, because of ampersands in the replacements
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;");
+    .replace(/&/g, "&amp;") // has to be first, because of ampersands in the replacements
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
 };
